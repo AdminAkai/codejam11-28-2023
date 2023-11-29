8c46419bdad62d7d176c455ddd46215894b6831c
Carson <carsonbarnes98@gmail.com>
2023-11-28 19:52:06 -0500

    math rand

--- 2023/2023-11-28/secret-santa-generator/index.js
+++ 2023/2023-11-28/secret-santa-generator/index.js
@@ -1,14 +1,17 @@
 const santaGenerator = (people) => {
   const peeps = {}
+  const mathRand = Math.floor(Math.random(0, people.length - 1))
   // map array of people to an object/hash map starting with empty object
   people.forEach(person => peeps[person])
   // loop through array and insert into object so we have names as keys
+    // for - in peeps object
   for (const person in peeps) {
     // random index
-  if (person === array[/* some random thing index */])
+  if (person === array[mathRand])
     peeps[person]
   }
-  // for - in peeps object
+
+
   // math.floor(math.rand(0, people.length - 1)) and use this number to index the array and assign to object key
   // check that current object name does not equal current array index
   // if true set key value
