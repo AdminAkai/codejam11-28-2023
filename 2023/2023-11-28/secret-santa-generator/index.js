d781c98e300a3be1c94f81e7e0bb9865dc5566a1
AdminAkai <akatoirotech@gmail.com>
2023-11-28 19:32:13 -0500

    added base code

---
+++ 2023/2023-11-28/secret-santa-generator/index.js
@@ -0,0 +1,11 @@
+const santaGenerator = (people) => {
+  const peeps = {}
+  // map array of people to an object/hash map starting with empty object
+  // loop through array and insert into object so we have names as keys
+  // for - in peeps object
+  // math.floor(math.rand(0, people.length - 1)) and use this number to index the array and assign to object key
+  // check that current object name does not equal current array index
+  // if true set key value
+  // if false randomize again
+  // return new hashmap of secret santa
+}
