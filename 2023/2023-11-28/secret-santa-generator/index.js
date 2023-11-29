4d207fc5199e3f26589d2b2d90d039162f66b69b
AdminAkai <akatoirotech@gmail.com>
2023-11-28 20:14:07 -0500

    finished algo

--- 2023/2023-11-28/secret-santa-generator/index.js
+++ 2023/2023-11-28/secret-santa-generator/index.js
@@ -1,20 +1,15 @@
 const santaGenerator = (people) => {
-  const peeps = {}
-  const mathRand = Math.floor(Math.random(0, people.length - 1))
-  // map array of people to an object/hash map starting with empty object
-  people.forEach(person => peeps[person])
-  // loop through array and insert into object so we have names as keys
-    // for - in peeps object
-  for (const person in peeps) {
-    // random index
-  if (person !== array[mathRand])
-    peeps[person]
+  const secretSantas = {}
+
+  people.forEach(person => secretSantas[person])
+
+  for (const person in secretSantas) {
+    const mathRand = Math.floor(Math.random(0, people.length - 1))
+    if (person !== array[mathRand]) {
+      secretSantas[person]
+    }
+    people.splice(mathRand, 1)
   }
 
-
-  // math.floor(math.rand(0, people.length - 1)) and use this number to index the array and assign to object key
-  // check that current object name does not equal current array index
-  // if true set key value
-  // if false randomize again
-  // return new hashmap of secret santa
+  return secretSantas
 }
